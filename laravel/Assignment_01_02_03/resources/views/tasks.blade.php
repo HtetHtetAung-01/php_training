f64a5b51f4f5327bcc4beca8a6668b45786a075d
HtetHtetAung-01 <htethtet.ucsy16@gmail.com>
2021-11-01 16:51:20 +0630

    assignment 03 added

---
+++ laravel/Assignment_01_02_03/resources/views/tasks.blade.php
@@ -0,0 +1,132 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="col-sm-offset-2 col-sm-8">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        New Task
+      </div>
+
+      <div class="panel-body">
+        <!-- Display Validation Errors -->
+        @include('common.errors')
+
+        <!-- New Task Form -->
+        <form action="{{ url('task')}}" method="POST" class="form-horizontal">
+          {{ csrf_field() }}
+
+          <!-- Task Name -->
+          <div class="form-group">
+            <label for="task-name" class="col-sm-3 control-label">Task</label>
+
+            <div class="col-sm-6">
+              <input type="text" name="name" id="task-name" class="form-control" value="{{ old('task') }}">
+            </div>
+          </div>
+
+          <div class="form-group">
+            <label for="task-deadline" class="col-sm-3 control-label">Deadline</label>
+            <div class="col-sm-6">
+              <input type="date" name="deadline" id="task-deadline" class="form-control" value="{{ old('task') }}">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="task-note" class="col-sm-3 control-label">Task Note</label>
+            <div class="col-sm-6">
+              <input type="text" name="note" id="task-note" class="form-control" value="{{ old('task') }}">
+            </div>
+          </div>
+
+          <!-- Add Task Button -->
+          <div class="form-group">
+            <div class="col-sm-offset-3 col-sm-6">
+              <button type="submit" class="btn btn-primary">
+                <i class="fa fa-btn fa-plus"></i>Add Task
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+    <!-- Current Tasks -->
+    @if (count($tasks) > 0)
+    <div class="panel panel-default">
+      <div class="panel-heading flex">
+        Current Tasks
+        <div class="csv-btn">
+          <a class="btn btn-primary upload-btn" href="importExportView">
+            <i class="fa fa-btn fa-upload"></i>Upload
+          </a>
+          <a class="btn btn-primary download-btn" href="export">
+            <i class="fa fa-btn fa-download"></i>Download
+          </a>
+        </div>
+      </div>
+
+      <div class="panel-body">
+        <form action="{{  url('/search')  }}" class="form-inline" method="get">
+          <label name="start-lb">Start Date : </label>
+          <input class="form-control" type="date" name="start" />&nbsp;
+          <label name="end-lb">End Date : </label>
+          <input class="form-control" type="date" name="end" /><br><br>
+
+          <label name="name-lb">Task Name : </label>
+          <input class="form-control" type="text" name="name" />
+          <button class="btn btn-success">Search</button>
+        </form>
+        <hr>
+        <table class="table table-striped task-table">
+          <thead>
+            <th>Task</th>
+            <th>Deadline</th>
+            <th>Task Notes</th>
+            <th>Created Date</th>
+            <th>&nbsp;</th>
+            <th>&nbsp;</th>
+          </thead>
+          <tbody>
+            @foreach ($tasks as $task)
+            <tr>
+              <td class="table-text">
+                <div>{{ $task->name }}</div>
+              </td>
+              <td class="table-text">
+                <div>{{ $task->deadline }}</div>
+              </td>
+              <td class="table-text">
+                <div>{{ $task->note }}</div>
+              </td>
+              <td class="table-text">
+                <div>{{ $task->created_at }}</div>
+              </td>
+              <!-- Task Update Button -->
+              <td>
+                <form action="/update/{{ $task->id }}" method="POST">
+                  {{ csrf_field() }}
+                  <button type="submit" class="btn btn-secondary">
+                    <i class="fa fa-btn fa-edit"></i>Update
+                  </button>
+                </form>
+              </td>
+              <!-- Task Delete Button -->
+              <td>
+                <form action="{{ url('task/'.$task->id) }}" method="POST" onSubmit="return confirm('Do you want to delete this task?')">
+                  {{ csrf_field() }}
+                  {{ method_field('DELETE') }}
+
+                  <button type="submit" class="btn btn-danger">
+                    <i class="fa fa-btn fa-trash"></i>Delete
+                  </button>
+                </form>
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+    @endif
+  </div>
+</div>
+@endsection
